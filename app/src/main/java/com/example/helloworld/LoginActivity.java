5d32f33079671a50d3ea760cd184e617ec106092
williamsebastianh <williamsebastianh@gmail.com>
2020-11-09 23:19:50 +0700

    Tugas7

--- app/src/main/java/com/example/helloworld/LoginActivity.java
+++ app/src/main/java/com/example/helloworld/LoginActivity.java
@@ -3,19 +3,24 @@ package com.example.helloworld;
 
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Html;
+import android.text.Spanned;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
-    private static final String TAG = LoginActivity.class.getSimpleName();
-    private EditText txtUsername;
-    private EditText txtPassword;
-    private Button btnlogin;
 
+    EditText TxUsername, TxPassword;
+    Button BtnLogin,button;
+    DatabaseHelper dbHelper;
+
+    SharedPrefManager sharedPrefManager;
 
 
 
@@ -23,19 +28,68 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        txtUsername = findViewById(R.id.txtUsername);
-        txtPassword = findViewById(R.id.txtPassword);
-        btnlogin = findViewById(R.id.btnlogin);
-        btnlogin.setOnClickListener(new View.OnClickListener() {
-            @Override
 
+
+        TxUsername = (EditText)findViewById(R.id.txUsername);
+        TxPassword = (EditText)findViewById(R.id.txPassword);
+        BtnLogin = (Button)findViewById(R.id.btnLogin);
+        dbHelper = new DatabaseHelper(this);
+        sharedPrefManager = new SharedPrefManager(this);
+
+
+        TextView tvCreateAccount = (TextView)findViewById(R.id.buatakun);
+        tvCreateAccount.setText(fromHtml("Tidak Punya Akun Bro? " +
+                "</font><font color='#3b5998'>Sini Buat</font>"));
+        tvCreateAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
             public void onClick(View v) {
-                if(txtPassword.getText().toString().equals("admin") && txtUsername.getText().toString().equals("admin")){
-                    Intent i = new Intent( LoginActivity.this, HomeActivity.class);
-                    Toast.makeText(getApplicationContext(), "Selamat Datang "+ txtUsername.getText(),   Toast.LENGTH_LONG).show();
-                    startActivity(i);
-                }else{
-                    Toast.makeText(getApplicationContext(),"Mohon Maaf Tidak Mengetahui "+ txtUsername.getText(), Toast.LENGTH_LONG).show();
-                }}
+                startActivity(new Intent(LoginActivity.this, Register.class));
+            }
         });
-    }}
+
+
+        if (sharedPrefManager.getSPSudahLogin()){
+            startActivity(new Intent(LoginActivity.this, HomeActivity.class)
+                    .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK));
+            finish();
+        }
+
+
+
+        BtnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = TxUsername.getText().toString().trim();
+                String password = TxPassword.getText().toString().trim();
+
+                Boolean res = dbHelper.checkUser(username,password);
+                if(res == true){
+
+                    Toast.makeText(LoginActivity.this, "Selamat Datang Bro", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(LoginActivity.this, HomeActivity.class));
+                  sharedPrefManager.saveSPBoolean(SharedPrefManager.SP_SUDAH_LOGIN, true);
+                    finish();
+                }
+                else
+                 {
+                    Toast.makeText(LoginActivity.this, "Coba Cek Dulu Email dan Password", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+//    public void Logout(View view){
+//        sharedPreferenceConfig.login_status(false);
+//        startActivity(new Intent(this,LoginActivity.class));
+//        finish();
+//    }
+
+    public static Spanned fromHtml(String html){
+        Spanned result;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N){
+            result = Html.fromHtml(html, Html.FROM_HTML_MODE_LEGACY);
+        }else {
+            result = Html.fromHtml(html);
+        }
+        return result;
+    }
+}
